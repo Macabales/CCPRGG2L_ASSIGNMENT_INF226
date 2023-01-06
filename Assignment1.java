936f11544dc4a06b56614203fc7412bc710d97f3
Macabales <119917680+Macabales@users.noreply.github.com>
2023-01-06 12:19:15 +0800

    Create Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,20 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class App {
+   
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your phone number: ");
+		String phoneNum = scan.nextLine();
+		
+		Pattern pattern = pattern.compile(regex: "[0][9]\d{9}") {
+		
+
+	}
+		
+}
+
+}
